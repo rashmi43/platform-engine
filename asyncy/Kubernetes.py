51176cd54c3a1575bcc6322da857c9f690a5b5c2
rashmikhanna <k.rashmi.r@gmail.com>
2018-11-14 03:42:01 -0800

    =fixed json string

--- asyncy/Kubernetes.py
+++ asyncy/Kubernetes.py
@@ -289,6 +289,8 @@ class Kubernetes:
                                 'volumeMounts': volMounts 
                              }],
                         'volumes': volumes
+                    }
+                }
             }
         }
 
